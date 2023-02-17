5de468a5b507b9a61b880992d903b12fdea47919
masudiharikrishna <masudiharikrishna@gmail.com>
2023-02-17 16:10:46 +0000

    solution

---
+++ src/components/RandomNumberGenerator/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+import {Component} from 'react'
+
+import './index.css'
+
+class RandomNumberGenerator extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <div>
+          <h1>Random Number</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RandomNumberGenerator
